7b021f399a95fbec6ca0d380eb5f72bb8c8fa65a
hansy <hansy.luo@gmail.com>
2016-10-31 22:58:10 +0800

    add main source file

---
+++ memchar.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+#include <linux/errno.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("Dual BSD/CPL");
+
+static int memchar_major, memchar_minor;
+
+static int memchar_init(void) {
+    printk(KERN_INFO "memchar module has been inited")
+    return 0;
+}
+
+static int memchar_exit(void) {
+    rintk(KERN_INFO "memchar module has been exited")
+    return;
+}
+
+module_init(memchar_init);
+module_exit(memchar_exit);
